09bfe6c4adf9c7f203f45c878baa1571f8a6a9b5
Gary Yang <gary.yang2000@yahoo.ca>
2016-03-22 13:10:32 -0400

    Multiples of 3 and 5
    If we list all the natural numbers below 10 that are multiples of 3 or
    5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    
    Find the sum of all the multiples of 3 or 5 below 1000.

---
+++ projectEuler_multiples of 3 and 5/projectEuler_multiples of 3 and 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler_multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sum = 0;
+            for (int i = 1; i <=333; i++)
+            {
+                sum += i * 3;
+                
+            }
+            for (int i = 1; i < 200; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    sum += i * 5;
+                }
+               
+            }
+            Console.WriteLine("the sum is {0}",sum);
+            Console.ReadLine();
+        }
+    }
+}
