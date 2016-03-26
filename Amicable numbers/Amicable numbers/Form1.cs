d41539121ff12289eacc1cbb938161c9c7eecf03
Gary Yang <gary.yang2000@yahoo.ca>
2016-03-25 22:41:25 -0400

    Amicable numbers
    Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
    If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
    
    For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
    
    Evaluate the sum of all the amicable numbers under 10000.

---
+++ Amicable numbers/Amicable numbers/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Amicable_numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum1;
+            int sum2;
+            int sum3=0;
+            for (int i = 10; i <= 10000; i++)
+            {
+                sum1 = numberOfDiv(i);
+                sum2 = numberOfDiv(sum1);
+                if ((i == sum2)&&(i!=sum1))
+                {
+                    textBox1.Text += "Amicable numbers found:" + i.ToString() +" and "+ sum1.ToString()+"\r\n";
+                    sum3 += i;
+                }
+
+            }
+            textBox1.Text += "\r\nSum is" + sum3.ToString();
+        }
+        public int numberOfDiv(int input)
+        {
+            int n = input;
+            int flag = 0;
+            int[] factors = new int[300];
+            int j = 0;
+            //int last = 0;
+            int tmp = 0;
+            int sum = 1;
+            //Following part finds out all the prime factors of n;
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    factors[j] = i;
+                    flag++;
+                    n = n / i;
+                    i--;
+                    j++;
+                }
+            }
+            flag = factors[0];
+            int time = 1;
+            tmp = 1;
+            // Console.Write("==>");
+            // sum of n's divisors:f(n)=(p1^0+p1^1+p1^2+…p1^a1)(p2^0+p2^1+p2^2+…p2^a2)…(pk^0+pk^1+pk^2+…pk^ak）
+            //num of n's divisors:f(n)=(a₁+1)(a₂+1)(a₃+1)…(ak+1)
+            for (int i = 1; i <= j; i++)
+            {
+
+                if (flag == factors[i])
+                {
+                    time++;
+                }
+                else
+                {
+                    tmp = 0;
+                    for (int k = 0; k <= time; k++)
+                    {
+                        tmp += (int)Math.Pow(flag, k);
+                    }
+                    sum *= tmp;
+                    time = 1;
+                    tmp = 1;
+                    flag = factors[i];
+                }
+            }
+            return sum - input;
+        }
+    }
+}
