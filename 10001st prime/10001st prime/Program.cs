c772c383f349d02ec2f0ec44e7b42839f9ab35ad
Gary Yang <gary.yang2000@yahoo.ca>
2016-03-25 22:40:33 -0400

    10001st prime
    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
    
    What is the 10 001st prime number?

---
+++ 10001st prime/10001st prime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10001st_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 7;
+            int i = 14;
+            while (counter < 10002)
+            {
+                if (judge_prime(i) != 0) 
+                { 
+                    Console.WriteLine("The {0} prime found {1}",counter, i); counter++; 
+                }
+                i++;
+            }
+
+            Console.WriteLine("The 10001st prime is {0}", i);
+            Console.ReadLine();
+        }
+        static public long judge_prime(long i)
+        {
+
+            for (long j = 2; j * j <= i; j++)
+            {
+                if (i % j == 0) return 0;
+            }
+            return i;
+        }
+    }
+}
